eafd9ff1cc2122dd385e9f202f20f72059cdc3e5
shreyasingh674 <shreya11993511@gmail.com>
2023-10-20 18:49:53 +0530

    Add initial files

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <h1> HELLO EVERYONE</h1>
+        <h2> Welcome to My First React App!</h2>
+        <h2>Myself Shreya Singh</h2>
+        <h2>I'm currently a second-year college student at Indraprastha College For Women</h2>
+        <h2>Delhi University</h2>
+        <h3>This is a basic introduction to React.js.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
